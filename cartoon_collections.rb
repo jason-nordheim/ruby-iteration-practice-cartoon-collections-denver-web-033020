a19f77ef26704d56f0eaa2c775b65de99b32bb7d
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-07 22:39:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls calls_long
   calls_long.any? { |x| x.length > 4  }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.find { |x| x == cheese  }
 end
